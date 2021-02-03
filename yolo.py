03015c7f7fa7307329da650a909e40f65fc18566
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-02-03 11:07:25 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -26,7 +26,7 @@ from utils.utils import (DecodeBox, bbox_iou, letterbox_image,
 #--------------------------------------------#
 class YOLO(object):
     _defaults = {
-        "model_path"        : 'model_data/yolov4_mobilenet_v1_map76.62.pth',
+        "model_path"        : 'model_data/yolov4_mobilenet_v1_voc.pth',
         "anchors_path"      : 'model_data/yolo_anchors.txt',
         "classes_path"      : 'model_data/voc_classes.txt',
         "backbone"          : 'mobilenetv1',
